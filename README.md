11019bbb1b3df3a90a7a4c1089ecc424e5d0f7b1
Brendan Weibel <BMWeibel@gmail.com>
2023-03-05 00:27:25 -0800

    Started persistence. Not working b/c pyinstaller

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Running
-Simply run the .exe in the `dist` folder and you will no longer be able to launch league until you stop the process from running in Task Manager. Pretty simple.
+Simply run the no-more-league.exe in the `dist` folder and you will no longer be able to launch league until you stop the process from running in Task Manager & delete the file from your startup folder.
+
+build script:
+`pyinstaller --noconsole --onefile -i "x.ico" .\no-more-league.py`
--- no-more-league.py
+++ no-more-league.py
@@ -1,22 +1,39 @@
 import psutil as psu
 from time import sleep
+from getpass import getuser
 from ctypes import windll
 from webbrowser import open as wbopen
+from shutil import copy
+DEBUG = False # solely for laziness, switch to False when actually building
+if DEBUG:
+    LEAGUE_CLIENT_POTENTIAL_NAMES = ['chrome.exe']
+else:
+    LEAGUE_CLIENT_POTENTIAL_NAMES = ['LolClient.exe', 'LeagueClient.exe']
+
+# Create persistence
+curr_user = getuser()
+curr_file = __file__
+startup_location = 'C:/Users/{uname}/AppData/Roaming/Microsoft/Windows/Start Menu/Programs/Startup'.format(uname=curr_user)
+copy(curr_file, startup_location)
+
+# main loop to check for league proc
 curr_proc = list(psu.win_service_iter())
-
-
-while(True):
-
-    curr_proc = list(psu.process_iter(['pid', 'name']))
+while(True): #lol
+    curr_proc = list(psu.process_iter(['pid', 'name', 'username']))
     in_list = False
     req_proc = None
+    # Checking procs for league
     for proc in curr_proc:
-        if proc.name() == 'LolClient.exe' or proc.name() == 'LeagueClient.exe': #TODO get actual league client name
-        #if proc.name() == 'firefox.exe': #Test line        
-            in_list = True
-            req_proc = proc
-            break
+        try:
+            proc_user = proc.username().split('\\')[-1]
+            if proc.name() in LEAGUE_CLIENT_POTENTIAL_NAMES and proc_user == req_user:        
+                in_list = True
+                req_proc = proc
+                break
+        except (PermissionError, psu.AccessDenied) as e:
+            pass
 
+    # do action to close LoL, then launch Leetcode
     if in_list:
         req_proc.kill()
         windll.user32.MessageBoxW(0, 'No More League, please.', 'Monkey', 0)
